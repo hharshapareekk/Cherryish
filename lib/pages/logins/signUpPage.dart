1b449a264f5eea1106d441dc9444b023db4e1826
Harsha Pareek <harshapareek14@gmail.com>
2024-04-13 15:37:44 +0530

    signup done

--- lib/pages/logins/signUpPage.dart
+++ lib/pages/logins/signUpPage.dart
@@ -1,5 +1,7 @@
-import 'package:cherryish/pages/homeScreen.dart';
+import 'package:cherryish/constants/globalVariables.dart';
 import 'package:cherryish/pages/logins/loginPage.dart';
+import 'package:cherryish/services/auth_service.dart';
+import 'package:cherryish/widgets/custom_textField.dart';
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatefulWidget {
@@ -11,15 +13,31 @@ class SignUpPage extends StatefulWidget {
 
 class _SignUpPageState extends State<SignUpPage> {
   late GlobalKey<FormState> _formkey; // Initialize _formkey
-
+  final _signUpFormKey = GlobalKey<FormState>();
+  final AuthService authService = AuthService();
   String email = '';
   String password = '';
   String username = '';
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     _formkey = GlobalKey<FormState>(); // Initialize _formkey
+  }
+   @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _usernameController.dispose();
+  }
+
+  void SignUpUser() {
+    authService.signUpUser(
+        context: context, email: email, username: username, password: password);
   }
 
   @override
@@ -85,129 +103,88 @@ class _SignUpPageState extends State<SignUpPage> {
                 height: 40.0,
               ),
               Container(
-                alignment: Alignment.centerLeft,
-                child: Text('                 Username'),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  child: TextFormField(
-                    key: ValueKey('email'),
-                    decoration: InputDecoration(
-                      enabledBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      prefixText: '   ',
-                      hintText: "  Enter your username or email",
-                    ),
-                    validator: (value) {
-                      if (value!.length < 3) { // Added null check for value
-                        return 'Username is small';
-                      } else {
-                        return null;
-                      }
-                    },
-                    onSaved: (value) {
-                      setState(() {
-                        username = value!;
-                      });
-                    },
-                  ),
-                  height: 50.0,
-                  width: 270.0,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.circular(100.0),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(color: Colors.black.withOpacity(0.7))
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                alignment: Alignment.centerLeft,
-                child: Text('                 Mail'),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  child: TextFormField(
-                    key: ValueKey('Mail id'),
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      enabledBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      prefixText: '   ',
-                      hintText: '   Enter Mail',
-                    ),
-                    validator: (value) {
-                      if (!(value!.contains('@'))) { // Added null check for value
-                        return 'invalid email';
-                      } else {
-                        return null;
-                      }
-                    },
-                    onSaved: (value) {
-                      setState(() {
-                        email = value!;
-                      });
-                    },
-                  ),
-                  height: 50.0,
-                  width: 270.0,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.circular(100.0),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(color: Colors.black.withOpacity(0.7))
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                alignment: Alignment.centerLeft,
-                child: Text('                 Password'),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  child: TextFormField(
-                    key: ValueKey('Password'),
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      enabledBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      prefixText: '   ',
-                      hintText: '   Enter Password',
-                    ),
-                    validator: (value) {
-                      if (value!.length < 6) { // Added null check for value
-                        return 'Password is small';
-                      } else {
-                        return null;
-                      }
-                    },
-                    onSaved: (value) {
-                      setState(() {
-                        password = value!;
-                      });
-                    },
-                  ),
-                  height: 50.0,
-                  width: 270.0,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.circular(100.0),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(color: Colors.black.withOpacity(0.7))
+                padding: const EdgeInsets.all(8),
+                color: GlobalVariables.backgroundColor,
+                child: Form(
+                  key: _signUpFormKey,
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 50.0,
+                        width: 270.0,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.circular(100.0),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(color: Colors.black.withOpacity(0.7))
+                          ],
+                        ),
+                        child: CustomTextField(
+                          controller: _usernameController,
+                          hintText: 'Username',
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Container(
+                        height: 50.0,
+                        width: 270.0,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.circular(100.0),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(color: Colors.black.withOpacity(0.7))
+                          ],
+                        ),
+                        child: CustomTextField(
+                          controller: _emailController,
+                          hintText: 'Email',
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Container(
+                        height: 50.0,
+                        width: 270.0,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.circular(100.0),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(color: Colors.black.withOpacity(0.7))
+                          ],
+                        ),
+                        child: CustomTextField(
+                          controller: _passwordController,
+                          hintText: 'Password',
+                        ),
+                      ),
+                      const SizedBox(height: 80),
+                      InkWell(
+                        child: Container(
+                          height: 70.0,
+                          width: 230.0,
+                          child: Center(
+                            child: Text(
+                              'SIGN UP',
+                              style: TextStyle(
+                                  fontSize: 40.0,
+                                  color: Colors.white,
+                                  letterSpacing: 5.0),
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(100.0),
+                            border: Border.all(color: Colors.white, width: 3.0),
+                          ),
+                        ),
+                        onTap: () {
+                          if (_signUpFormKey.currentState!.validate()) {
+                            SignUpUser();
+                          }
+                        },
+                      )
                     ],
                   ),
                 ),
@@ -234,19 +211,7 @@ class _SignUpPageState extends State<SignUpPage> {
                     border: Border.all(color: Colors.white, width: 3.0),
                   ),
                 ),
-                onTap: () {
-  if (_formkey.currentState!.validate()) {
-    _formkey.currentState!.save();
-    if (email.isNotEmpty && password.isNotEmpty) { // Check if email and password are not empty
-      Navigator.pop(context);
-      Navigator.push(context,
-          MaterialPageRoute(builder: (context) => HomeScreen()));
-    } else {
-      print('Email or password is empty.');
-    }
-  }
-}, 
-
+                onTap: () {},
               )
             ],
           ),
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,10 +1,14 @@
+import 'package:cherryish/constants/error_handling.dart';
 import 'package:cherryish/constants/globalVariables.dart';
+import 'package:cherryish/constants/utils.dart';
 import 'package:cherryish/models/user.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService {
   //signup user
   void signUpUser({
+    required BuildContext context,
     required String email,
     required String username,
     required String password,
@@ -25,7 +29,11 @@ class AuthService {
           'Content-Type': 'application/json; charset=UTF-8',
         },
       );
-      
-    } catch (e) {}
+      httpErrorHandlingFn(response: res, context: context, onSuccess: (){
+        showSnackBar(context, 'Account Has Been Created! Login With Same Credentials');
+      },);
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
   }
 }
